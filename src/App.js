d65cd8908e0acc9d70f1b3a4afb3bfcd04f4dbcc
ANAGIT22 <103462670+ANAGIT22@users.noreply.github.com>
2022-09-02 15:57:24 -0300

    <mensaje>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import 'bulma/css/bulma.min.css';
 import './App.css';
 import {BrowserRouter , Routes ,Route } from 'react-router-dom';
 import Home from './pages/home';
 import Game from './pages/game';
+
+
+
 function App() {
 
   return (
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,8 +1,67 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import QuestionCard from "../../components/QuestionCard"
 
+const API_URL = "https://62bb6e36573ca8f83298fbef.mockapi.io/metcampweb22/v1/questions/harry-potter";
 
 function Game () {
+    const [loading, setLoading ] = useState(true);
+    const [questions, setQuestions] = useState([]);
+// quiero que llame a las preguntas cuando la persona entre al  Juego.
+useEffect(() => {
+    
+    fetch(API_URL) 
+    .then(response => response.json())
+    .then(data => setQuestions(data)) // recibe los datos de la Api
+    .catch(error => console.log(error)) // solo va a ejecurase si hay un error
+    .finally (() => setLoading (false)) // finalmente termina de cargar.
+
+
+
+}, [])
+
     return (
-        <h1> Esta es la Page del juego</h1>
+        <div className="container">
+            <section className="section">
+            <nav className="breadcrumb" aria-label="breadcrumbs">
+  <ul>
+    <li>
+        <Link to ="/">Inicio</Link>
+    </li>
+    <li className="is-active">
+        <Link to = "/game">Juego</Link>
+
+    </li>
+    
+    
+  </ul>
+</nav>
+{
+    loading && 
+        <div>Cargando...</div>
+    
+
+}
+{
+    !loading && (
+        <form>
+       {
+    
+         questions.map ((pregunta)   => {
+            return <QuestionCard key={pregunta.id} preguntaActual = {pregunta} />
+
+           
+         })
+        }
+
+  </form>
+    )
+}
+<h1>El Juego</h1>
+            </section>
+        </div>
+
+        
     )
     
 }
