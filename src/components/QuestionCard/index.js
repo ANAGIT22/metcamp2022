f519c83153e3ea250b0ff5108cc3008fed478ed0
ANAGIT22 <103462670+ANAGIT22@users.noreply.github.com>
2022-09-09 19:33:03 -0300

    se tocan estilos

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -1,27 +1,53 @@
-function QuestionCard({preguntaActual }) {
+import "../../index.css";
+
+function QuestionCard({ preguntaActual, selectedAnswers, setSelectedAnswers, mostrarResultado }) {
+
+    function seleccionarRespuesta(identificador, valorOpcion) {//identifica la pregunta y si la respuesta es correcta o inc
+        const otrasRespuestas = selectedAnswers.filter((respuesta) => respuesta.id !== identificador)
+        setSelectedAnswers([...otrasRespuestas,
+        {
+            id: identificador,
+            valorOpcion
+        }
+        ])
+    }
+
+    function mostrarColores(valor) {
+        let valorClase = "";
+        if (mostrarResultado) {
+            if (valor === true) {
+                valorClase = "has-text-primary";
+            } else {
+                valorClase = "has-text-danger"
+            }
+        }
+        return valorClase;
+    }
+
     return (
         <div className="box">
-            <div style={{paddingBotton:"10px"}}>
-            <span className="tag is-rounded is-info"> {preguntaActual.id} </span>
-           <span><strong>{preguntaActual.question}</strong></span>
-           </div>
-           <br />
-         
+            <div className="mi-clase">
+                <span className="tag is-rounded is-primary is-light">{preguntaActual.id}</span>
+                <span><strong> {preguntaActual.question}</strong></span>
+            </div>
             {
-               preguntaActual.answers.map((opcion) => (
-                <div key={opcion.id}> 
-                <input type= "radio" id={`${opcion.id}`}  name={opcion.id} value={opcion.answer} ></input>
-                <label htmlFor={`${opcion.id}`}>{opcion.answer}</label>
-                
-                 </div>
-           ))
-
-        }
-     
-         
+                preguntaActual.answers.map((opcion) => (
+                    <div
+                        key={opcion.id}
+                        onChange={() => seleccionarRespuesta(preguntaActual.id, opcion.is_correct)}
+                    >
+                        <input type="radio"
+                            id={`${opcion.id}`}
+                            name={preguntaActual.id}
+                            value={opcion.answer} ></input>
+                        <label htmlFor={`${opcion.id}`}
+                            className={mostrarColores(opcion.is_correct)}
+                        >&nbsp;{opcion.answer}</label>
+                    </div>
+                ))
+            }
         </div>
     )
-
 }
 
 export default QuestionCard;
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,68 +1,76 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Button from "../../components/Button";
 import QuestionCard from "../../components/QuestionCard"
+import Result from "../../components/Result";
+import useFetch from "../../hooks/useFetch";
 
 const API_URL = "https://62bb6e36573ca8f83298fbef.mockapi.io/metcampweb22/v1/questions/harry-potter";
 
-function Game () {
-    const [loading, setLoading ] = useState(true);
-    const [questions, setQuestions] = useState([]);
-// quiero que llame a las preguntas cuando la persona entre al  Juego.
-useEffect(() => {
-    
-    fetch(API_URL) 
-    .then(response => response.json())
-    .then(data => setQuestions(data)) // recibe los datos de la Api
-    .catch(error => console.log(error)) // solo va a ejecurase si hay un error
-    .finally (() => setLoading (false)) // finalmente termina de cargar.
+function Game() {
+    const { loading, data: questions } = useFetch(API_URL);
+    const [selectedAnswers, setSelectedAnswers] = useState([]); // Guarda las respuestas 
+    const [result, setResult] = useState(0);
+    const [mostrarResultado, setMostrarResultado] = useState(false);
 
-
-
-}, [])
+    function calcularResultado() {
+        const respuestasCorrectas = selectedAnswers.filter((respuesta) => respuesta.valorOpcion === true)
+        setResult(respuestasCorrectas.length)
+        setMostrarResultado(true)
+    }
 
     return (
         <div className="container">
             <section className="section">
-            <nav className="breadcrumb" aria-label="breadcrumbs">
-  <ul>
-    <li>
-        <Link to ="/">Inicio</Link>
-    </li>
-    <li className="is-active">
-        <Link to = "/game">Juego</Link>
+                <nav className="breadcrumb" aria-label="breadcrumbs">
+                    <ul>
+                        <li>
+                            <Link to="/">Inicio</Link>
+                        </li>
+                        <li className="is-active">
+                            <Link to="/game">Juego</Link>
+                        </li>
+                    </ul>
+                </nav>
+                {
+                    loading &&
+                    <div>Cargando...</div>
+                }
+                {
+                    !loading && (
+                        <form>
+                            {
+                                questions.map((pregunta) => {
+                                    return <QuestionCard
+                                        key={pregunta.id}
+                                        preguntaActual={pregunta}
+                                        selectedAnswers={selectedAnswers} //pasan las respuestas
+                                        setSelectedAnswers={setSelectedAnswers} //guardar respuestas selec
+                                        mostrarResultado={mostrarResultado}
+                                    />
+                                })
+                            }
+                        </form>
+                    )
+                }
+                <div className="level">
+                    <div className="level-left">
+                        {
+                            mostrarResultado &&
+                            <Result valorResultado={result} />
+                        }
+                    </div>
+                    <div className="level-right">
+                        <Button
+                            disabled ={selectedAnswers.length !== questions.length || mostrarResultado}
+                            onClick={() => calcularResultado()}
+                            text="Validar" />
 
-    </li>
-    
-    
-  </ul>
-</nav>
-{
-    loading && 
-        <div>Cargando...</div>
-    
-
-}
-{
-    !loading && (
-        <form>
-       {
-    
-         questions.map ((pregunta)   => {
-            return <QuestionCard key={pregunta.id} preguntaActual = {pregunta} />
-
-           
-         })
-        }
-
-  </form>
-    )
-}
-<h1>El Juego</h1>
+                    </div>
+                </div>
             </section>
         </div>
-
-        
     )
-    
 }
+
 export default Game;
